3342bf90aab358ece30036750db714681be3b6ba
Dan J Miller <danjm.com@gmail.com>
2020-07-16 12:45:28 -0230

    update 15

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log(123);
 window.location = 'https://github.com';
+	window.ReactNativeWebView && window.ReactNativeWebView.postMessage(JSON.stringify(
+		{
+			type: 'GET_WEBVIEW_URL',
+			payload: {
+				url: 'https://google.com'
+			}
+		}
+	))
 console.log(4567);
