5dc25bf8548a1204e7fbe62aead58c06f24f84e3
Dan J Miller <danjm.com@gmail.com>
2020-07-16 16:20:57 -0230

    update 30

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 console.log(123);
-window.ReactNativeWebView && window.ReactNativeWebView.postMessage = (...args) => {
-	console.log('args', args)
-	return window.ReactNativeWebView.postMessage(...args)
-	false && window.ReactNativeWebView.postMessage(JSON.stringify(
-		{
-			type: 'GET_WEBVIEW_URL',
-			payload: {
-				url: 'https://google.com'
-			}
-		}
-	))
-}
-window.location = 'https://google.com'
+fetch('http://clients1.google.com/generate_204')
+.then((...args) => console.log('args', args))
 console.log(4567);
