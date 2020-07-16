29104bf32eea53acc752fd53044febbb6e5bcfcb
Dan J Miller <danjm.com@gmail.com>
2020-07-16 17:24:45 -0230

    update 70

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log(999);
-fetch('https://clients1.google.com/generate_204',{ mode: 'no-cors'})
-.then((...args) => console.log('args33', args))
+window.location = 'http://httpstat.us/302';
 console.log(999);
