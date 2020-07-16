07fbf413ac43e31be984dc344cfa7f17ecff1c7c
Dan J Miller <danjm.com@gmail.com>
2020-07-16 16:45:53 -0230

    update 48

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-console.log(123);
-window.ReactNativeWebView && window.ReactNativeWebView.postMessage = (...args) => {
-  console.log('args24', args)
-  return window.ReactNativeWebView.postMessage(...args)
+console.log(555);
+if (window.ReactNativeWebView) {
+  window.ReactNativeWebView.postMessage = (...args) => {
+      console.log('args24', args)
+      return window.ReactNativeWebView.postMessage(...args)
+  }
 }
 window.location = 'https://google.com'
-console.log(45678);
+console.log(777);
